4faebbcd3d6edc9cf47fc5dc74dd8984a7865825
OAnt <antoine@orozco.fr>
2013-11-22 15:52:21 +0100

    better interface

--- ChatClient/__main__.py
+++ ChatClient/__main__.py
@@ -2,4 +2,17 @@ __author__ = 'Antoine'
 import chat_client
 import sys
 
-chat_client.ChatClient("localhost", 4000, sys.stdin, sys.stdout)
+
+def start_chat(arg_list, pipe_in, pipe_out):
+    arg_list.pop(0)
+    if len(arg_list) == 2:
+        host = arg_list[0]
+        try:
+            port = int(arg_list[1])
+        except e:
+            return
+        chat_client.ChatClient(host, port, pipe_in, pipe_out)  # open("ChatClient/get_req", 'r')
+    else:
+        print "usage: python ChatClient <host> <port>"
+
+start_chat(sys.argv, sys.stdin, sys.stdout)
--- ChatClient/chat_client.py
+++ ChatClient/chat_client.py
@@ -2,22 +2,23 @@ import gevent
 import gevent.socket
 from gevent import select
 
+
 class ChatClient(object):
 
     def __init__(self, host, port, pipe_in, pipe_out):
         try:
-            self.Socket = gevent.socket.create_connection((host, port))
+            self.socket = gevent.socket.create_connection((host, port))
         except IOError:
             print "Server not ready:", host, port
             exit(2)
         self.pipe_out = pipe_out
         self.pipe_in = pipe_in
-        self.dispatcher = {self.pipe_in: self.write_connection, self.Socket: self.listen_connection}
+        self.dispatcher = {self.pipe_in: self.write_connection, self.socket: self.listen_connection}
         gevent.joinall([gevent.spawn(self.scheduler)])
 
     def listen_connection(self, zzz):
         try:
-            self.pipe_out.write(self.Socket.recv(1024))
+            self.pipe_out.write(self.socket.recv(1024))
             self.pipe_out.flush()
         except IOError as e:
             raise gevent.GreenletExit
@@ -25,13 +26,13 @@ class ChatClient(object):
     def write_connection(self, a_pipe):
         line = a_pipe.readline()
         try:
-            self.Socket.sendall(line)
+            self.socket.sendall(line)
         except IOError:
             raise gevent.GreenletExit
 
     def scheduler(self):
         while True:
-            to_read, to_write, to_err = select.select(self.dispatcher.keys(),[],[],1)
-            for input in to_read:
-                self.dispatcher[input](input)
+            to_read, to_write, to_err = select.select(self.dispatcher.keys(), [], [], 1)
+            for an_input in to_read:
+                self.dispatcher[an_input](an_input)
 
